a2625dbaea886b1fa3a95aea3c9fa38d4dbb2370
simonpunk <simonpunk2016@gmail.com>
2026-03-20 01:59:32 +0800

    Refined OPEN_REDIRECT feature
    !!! This commit requires the updated ksu_susfs binary !!!
    
    - Implement SRCU for safe read/write.
    
    - Maximize the stealthiness by placing more hooks.
    
    - Update ksu_susfs cli, add_open_redirect now requires one more arg "uid_scheme" to make effective on process with specific uid / uid range.
    
    - Update ksu_susfs binary and module templetes.

--- ksu_module_susfs/boot-completed.sh
+++ ksu_module_susfs/boot-completed.sh
@@ -16,36 +16,51 @@ if [ ! -f "/data/adb/susfs_no_auto_add_kernel_umount" ]; then
 fi
 EOF
 
-#### Hide some sus paths, effective only for processes that are marked umounted with uid >= 10000 ####
+#### Hide path like /sdcard/<target_root_dir> from all user app processes without root access ####
 cat <<EOF >/dev/null
 ## First we need to wait until files are accessible in /sdcard ##
 until [ -d "/sdcard/Android" ]; do sleep 1; done
 
-## For paths that are read-only all the time, add them via 'add_sus_path' ##
-${SUSFS_BIN} add_sus_path /sys/block/loop0
-${SUSFS_BIN} add_sus_path /system/addon.d
-${SUSFS_BIN} add_sus_path /vendor/bin/install-recovery.sh
-${SUSFS_BIN} add_sus_path /system/bin/install-recovery.sh
+## Next we need to set the path of /sdcard/ to tell kernel where the actual /sdcard is ##
+ksu_susfs set_sdcard_root_path /sdcard
 
-## For paths that are frequently modified, we can add them via 'add_sus_path_loop' ##
-${SUSFS_BIN} add_sus_path_loop /sdcard/TWRP
-${SUSFS_BIN} add_sus_path_loop /sdcard/MT2
-${SUSFS_BIN} add_sus_path_loop /sdcard/AppManager
-${SUSFS_BIN} add_sus_path_loop /sdcard/Android/data/io.github.muntashirakon.AppManager
-${SUSFS_BIN} add_sus_path_loop /sdcard/Android/media/io.github.muntashirakon.AppManager
-${SUSFS_BIN} add_sus_path_loop /data/local/tmp/main.jar
+## Now we can add the path ##
+ksu_susfs add_sus_path /sdcard/TWRP
+ksu_susfs add_sus_path /sdcard/MT2
+
+## Please note that sometimes the path needs to be added twice or above to be effective ##
+## Besides, all user apps without root access cannot see the hidden path '/sdcard/<hidden_path>' unless you grant it root access ##
+EOF
+
+#### Hide the leaking app path like /sdcard/Android/data/<app_package_name> from syscall ####
+cat <<EOF >/dev/null
+## First we need to wait until files are accessible in /sdcard ##
+until [ -d "/sdcard/Android" ]; do sleep 1; done
+
+## Next we need to set the path of /sdcard/ to tell kernel where the actual /sdcard/Android/data is ##
+ksu_susfs set_android_data_root_path /sdcard/Android/data
+
+## Now we can add the path ##
+ksu_susfs add_sus_path /sdcard/Android/data/bin.mt.plus
+EOF
+
+#### For path that needs to be re-flagged as SUS_PATH on each non-root user app / isolated service starts via add_sus_path_loop ####
+cat <<EOF >/dev/null
+## - Path added via add_sus_path_loop will be re-flagged as SUS_PATH on each non-root process / isolated service starts ##
+## - This can help ensure some path that keep its inode status reset for whatever reason to be flagged as SUS_PATH again ##
+## - Please also note that only paths NOT inside '/sdcard/' or '/storage/' can be added via add_sus_path_loop ##
+## - ONLY USE THIS WHEN NECCESSARY !!! ##
+ksu_susfs add_sus_path_loop /sys/block/loop0
 EOF
 
 
-#### Hide the mmapped real file from various maps in /proc/self/, effective only for processes that are marked umounted with uid >= 10000 ####
+#### Hide the mmapped real file from various maps in /proc/self/ ####
 cat <<EOF >/dev/null
-## - *Please note that it is better to do it in boot-completed starge
+## - Please note that it is better to do it in boot-completed starge
 ##   Since some target path may be mounted by ksu, and make sure the
 ##   target path has the same dev number as the one in global mnt ns,
-##   otherwise the sus map flag won't be seen on the umounted proocess.
-## - *Besides, if the source files get umounted and stay only in like zygote's memory maps,
-##   then it will not work as well since sus_map checks for real file's inode.
-## - To debug the namespace issue, users can do this in a root shell:
+##   otherwise the sus map flag won't be seen on the umounted process.
+## - To debug with this, users can do this in a root shell:
 ##   1. Find the pid and uid of a opened umounted app by running
 ##      ps -enf | grep myapp
 ##   2. cat /proc/<pid_of_myapp>/maps | grep "<added/sus_map/path>"'
@@ -58,16 +73,16 @@ cat <<EOF >/dev/null
 ##         busybox nsenter -t <pid_of_mnt_ns_the_target_dev_number_belongs_to> -m ksu_susfs add_sus_map <target_path>
 
 ## Hide some zygisk modules ##
-${SUSFS_BIN} add_sus_map /data/adb/modules/my_module/zygisk/arm64-v8a.so
+ksu_susfs add_sus_map /data/adb/modules/my_module/zygisk/arm64-v8a.so
 
 ## Hide some map traces caused by some font modules ##
-${SUSFS_BIN} add_sus_map /system/fonts/Roboto-Regular.ttf
-${SUSFS_BIN} add_sus_map /system/fonts/RobotoStatic-Regular.ttf
+ksu_susfs add_sus_map /system/fonts/Roboto-Regular.ttf
+ksu_susfs add_sus_map /system/fonts/RobotoStatic-Regular.ttf
 EOF
 
-#### Unhide all sus mounts from /proc/self/[mounts|mountinfo|mountstat] for NON-SU processes ####
+#### Unhide all sus mounts from /proc/self/[mounts|mountinfo|mountstat] for non-su processes ####
 ## It is suggested to unhide it in this stage, and let kernel or zygisk to umount them for user processes, but this is up to you ##
 cat <<EOF >/dev/null
-${SUSFS_BIN} hide_sus_mnts_for_non_su_procs 0
+ksu_susfs hide_sus_mnts_for_non_su_procs 0
 EOF
 

6c2929917dd4f96d3516ffad9a8c2228d38b2ac3
simonpunk <simonpunk2016@gmail.com>
2026-03-26 23:28:41 +0800

    ksu_module_susfs: Update templates

--- ksu_module_susfs/service.sh
+++ ksu_module_susfs/service.sh
@@ -7,16 +7,13 @@ SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 
 source ${MODDIR}/utils.sh
 
-## Hexpatch prop name for newer pixel device ##
+## Delete some prop names for newer pixel device ##
 cat <<EOF >/dev/null
-# Remember the length of search value and replace value has to be the same #
-resetprop -n "ro.boot.verifiedbooterror" "0"
-susfs_hexpatch_prop_name "ro.boot.verifiedbooterror" "verifiedbooterror" "hello_my_newworld"
-
-resetprop -n "ro.boot.verifyerrorpart" "true"
-susfs_hexpatch_prop_name "ro.boot.verifyerrorpart" "verifyerrorpart" "letsgopartyyeah"
-
+resetprop --delete "ro.boot.verifiedbooterror"
+resetprop --delete "ro.boot.verifyerrorpart"
 resetprop --delete "crashrecovery.rescue_boot_count"
+## Remember to compat the hole after prop deletion (require updated resetprop)##
+resetprop -c
 EOF
 
 ## Undo hiding sus mounts for all non-su processes ##
